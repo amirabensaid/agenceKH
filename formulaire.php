3c43ec5878acd1f3bfe057cbdc8b339266c421f7
Amira Bensaid <entredcollege2020@hotmail.com>
2022-02-19 16:03:03 +0100

    Delete formulaire.php

--- formulaire.php
+++
@@ -1,8 +0,0 @@
-<form method="POST"  action="">
-id:<input type="text" name="id"></br>
-nom:<input type="text" name="nom"></br>
-nbetoil:<input type="text" name="nbetoil"></br>
-ville:<input type="text" name="ville"></br>
-image:<input type="text" name="image"></br>
-<input type="submit" name="ajout" value="ajouter">
-</form>
