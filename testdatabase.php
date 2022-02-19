214f2ed33503677cc21d534ba94c25ad73b3f426
Amira Bensaid <entredcollege2020@hotmail.com>
2022-02-19 16:05:13 +0100

    Delete testdatabase.php

--- testdatabase.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once "model/database.php";
-$db = new database();
-$rq = "select id,nom,description from hotel";
-$hotel = $db->query($rq);
-echo '<pre>';
-print_r($hotel);
-echo '<pre>';
